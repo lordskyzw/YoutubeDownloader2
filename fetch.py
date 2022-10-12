7e37b607ab80fd5b376f186c7b5a9910d92f20c4
Tarmica <lilskyforever@gmail.com>
2022-10-12 20:58:10 +0200

    Owm_attributes("-transparentcolor") is problematic

--- fetch.py
+++ fetch.py
@@ -2,83 +2,84 @@ import os
 import tkinter
 from pytube import YouTube
 
+name = os.getlogin()
+download_path = f'C:\\Users\\{name}\\Downloads'
+
 root = tkinter.Tk()
 root.iconbitmap('./media/icon.ico')
 root.title("Fetch")
 root.geometry("300x300")
 
 
-frame = tkinter.Frame(root, width=300, height=300)
 
-screen = tkinter.Entry(frame, width=50, borderwidth=5, font=("Helvetica", 12), bg='white', fg='black')
+
+screen = tkinter.Entry(root, width=50, borderwidth=5, font=("Helvetica", 12), bg='white', fg='black')
 screen.insert(0, "Enter YouTube link")
 screen.bind('<FocusIn>', lambda args: screen.delete('0', 'end'))
 screen.pack()
 
-name = os.getlogin()
-download_path = f'C:\\Users\\{name}\\Downloads'
+
 
 def download_video():
     confirmation = tkinter.Label(root, text=f'Saving to {download_path}', bg='white', fg='black')
-    no_link_label = tkinter.Label(frame, text='No link inserted!', bg='white', fg='red', font=("Helvetica", 24))
+    no_link_label = tkinter.Label(root, text='No link inserted!')
+    complete_lablel = tkinter.Label(root, text="Download Complete & saved in %s" %download_path)
     def close():
-        frame.after(2000, no_link_label.destroy())
-        frame.after(2000, confirmation.destroy())
+        root.after(2000, no_link_label.destroy())
+        root.after(2000, confirmation.destroy())
+        root.after(2000, complete_lablel.destroy())
         
     def download_start():
-        
         confirmation.pack()
         close()
         
     def download_complete():
-        complete_lablel = tkinter.Label(frame, text="Download Complete & saved in %s" %download_path)
         complete_lablel.pack()
         close()
         
     try:
         video = screen.get()
-        YouTube(video, on_progress_callback=download_start() , on_complete_callback=download_complete()).streams.get_highest_resolution().download(str(download_path))
+        YouTube(video, on_progress_callback=download_start , on_complete_callback=download_complete).streams.get_highest_resolution().download(str(download_path))
         
     except:  
         no_link_label.pack()
         close()
         
 def download_audio():
+    confirmation = tkinter.Label(root, text=f'Saving to {download_path}', bg='white', fg='black')
+    no_link_label = tkinter.Label(root, text='No link inserted!', bg='grey', fg='black', font=("Helvetica", 8))
+    complete_lablel = tkinter.Label(root, text="Download Complete & saved in %s" %download_path)
         
     def close():
-        no_link_label = tkinter.Label(frame, text='No link inserted!', bg='grey', fg='black', font=("Helvetica", 8))
-        no_link_label.pack()
-        frame.after(2000, no_link_label.destroy())
+        root.after(2000, confirmation.destroy())
+        root.after(2000, no_link_label.destroy())
         
     def download_start():
-        confirmation = tkinter.Label(root, text=f'Saving to {download_path}', bg='white', fg='black')
         confirmation.pack()
         close()
         
     def download_complete():
-        complete_lablel = tkinter.Label(frame, text="Download Complete & saved in %s" %download_path)
         complete_lablel.pack()
         close()
         
         
-        try:
-            audio = screen.get()
-            YouTube(audio, on_progress_callback=download_start(), on_complete_callback=download_complete()).streams.get_audio_only(subtype="mp3").download(str(download_path))
-        except:
-            close()
+    try:
+        audio = screen.get()
+        YouTube(audio, on_progress_callback=download_start, on_complete_callback=download_complete).streams.get_audio_only(subtype="mp3").download(str(download_path))
+    except:
+        no_link_label.pack()
+        close()
         
-downloadvid_button = tkinter.Button(frame, relief='raised', borderwidth=5, command=download_video, text="Download Video", highlightcolor="red", width=20, height=2)
+downloadvid_button = tkinter.Button(root, relief='raised', borderwidth=5, command=download_video, text="Download Video", highlightcolor="red", width=20, height=2)
 downloadvid_button.pack()
-downloadaud_button = tkinter.Button(frame, relief='raised', borderwidth=5, command=download_audio, text="Download Audio", highlightcolor="red", width=20, height=2)
+downloadaud_button = tkinter.Button(root, relief='raised', borderwidth=5, command=download_audio, text="Download Audio", highlightcolor="red", width=20, height=2)
 downloadaud_button.pack()
 footnote = tkinter.Label(root, text="Made by Engineer Chiwara", bg='black', fg='grey', font=("Helvetica", 8))
 footnote.pack(side=tkinter.BOTTOM)
-frame.pack()
-root.wm_attributes("-transparentcolor", "white")
+
+
+root.wm_resizable(width=False, height=False)
+
+#root.wm_attributes("-transparentcolor", "white")
 root.wm_attributes("-alpha", 0.9)
-
-
-
-
-
 root.mainloop()

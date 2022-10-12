7a163987fc549caf97bd06799ab40e9e47e0ccf7
Tarmica <lilskyforever@gmail.com>
2022-10-12 18:03:00 +0200

    ok the frame is off

--- fetch.py
+++ fetch.py
@@ -19,49 +19,66 @@ name = os.getlogin()
 download_path = f'C:\\Users\\{name}\\Downloads'
 
 def download_video():
+    confirmation = tkinter.Label(root, text=f'Saving to {download_path}', bg='white', fg='black')
+    no_link_label = tkinter.Label(frame, text='No link inserted!', bg='white', fg='red', font=("Helvetica", 24))
     def close():
         frame.after(2000, no_link_label.destroy())
-    
-    def download_complete():
+        frame.after(2000, confirmation.destroy())
+        
+    def download_start():
+        
+        confirmation.pack()
         close()
+        
+    def download_complete():
         complete_lablel = tkinter.Label(frame, text="Download Complete & saved in %s" %download_path)
         complete_lablel.pack()
         close()
         
     try:
         video = screen.get()
-        YouTube(video, on_complete_callback=download_complete()).streams.get_highest_resolution().download(str(download_path))
-        confirmation = tkinter.Label(root, text=f'Saving to {download_path}')
-        confirmation.pack()
+        YouTube(video, on_progress_callback=download_start() , on_complete_callback=download_complete()).streams.get_highest_resolution().download(str(download_path))
+        
     except:  
-        no_link_label = tkinter.Label(frame, text='No link inserted!', bg='white', fg='red', font=("Helvetica", 24))
         no_link_label.pack()
         close()
         
 def download_audio():
+        
     def close():
+        no_link_label = tkinter.Label(frame, text='No link inserted!', bg='grey', fg='black', font=("Helvetica", 8))
+        no_link_label.pack()
         frame.after(2000, no_link_label.destroy())
         
-    try:
-        audio = screen.get()
-        YouTube(audio).streams.get_audio_only(subtype="mp3").download(str(download_path))
-    except:
-        no_link_label = tkinter.Label(frame, text='No link inserted!', bg='white', fg='red', font=("Helvetica", 24))
-        no_link_label.pack()
+    def download_start():
+        confirmation = tkinter.Label(root, text=f'Saving to {download_path}', bg='white', fg='black')
+        confirmation.pack()
         close()
         
-downloadvid_button = tkinter.Button(frame, command=download_video, text="Download Video", highlightcolor="red", width=20, height=2)
+    def download_complete():
+        complete_lablel = tkinter.Label(frame, text="Download Complete & saved in %s" %download_path)
+        complete_lablel.pack()
+        close()
+        
+        
+        try:
+            audio = screen.get()
+            YouTube(audio, on_progress_callback=download_start(), on_complete_callback=download_complete()).streams.get_audio_only(subtype="mp3").download(str(download_path))
+        except:
+            close()
+        
+downloadvid_button = tkinter.Button(frame, relief='raised', borderwidth=5, command=download_video, text="Download Video", highlightcolor="red", width=20, height=2)
 downloadvid_button.pack()
-downloadaud_button = tkinter.Button(frame, command=download_audio, text="Download Audio", highlightcolor="red", width=20, height=2)
+downloadaud_button = tkinter.Button(frame, relief='raised', borderwidth=5, command=download_audio, text="Download Audio", highlightcolor="red", width=20, height=2)
 downloadaud_button.pack()
-footnote = tkinter.Label(root, text="Made by Engineer Chiwara", bg='white', fg='black', font=("Helvetica", 8))
+footnote = tkinter.Label(root, text="Made by Engineer Chiwara", bg='black', fg='grey', font=("Helvetica", 8))
 footnote.pack(side=tkinter.BOTTOM)
-
-
-root.wm_attributes("-transparentcolor", "white")
-
-
-
-
 frame.pack()
+root.wm_attributes("-transparentcolor", "white")
+root.wm_attributes("-alpha", 0.9)
+
+
+
+
+
 root.mainloop()

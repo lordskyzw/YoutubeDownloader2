327f151aaccd3b178caa2437ae782789b5aa851e
Tarmica <lilskyforever@gmail.com>
2022-10-15 19:59:02 +0200

    Going Compact

--- test.py
+++
@@ -1,3 +0,0 @@
-from pytube import YouTube
-
-YouTube('https://www.youtube.com/watch?v=9bZkp7q19f0', proxies={'HTTPS': '81.171.24.199:3128'}).streams.get_highest_resolution().download('C:\\Users\\user\\Downloads')
